12f4ff2ab4c9c0df7ee0ad1401f68d41c7cd997f
valerio <valerio.deprosperi@gmail.com>
2020-10-25 12:08:44 +0100

    shoe1 e 2

--- index.php
+++ index.php
@@ -5,11 +5,24 @@ class shoes{
 }
 
 class type extends shoes{
-  public $brandName;
+  public $shoeColour;
   public $shoeModel;
   public $shoeNumber;
 
-
-
 }
+
+$shoe1 = new type();
+$shoe1->brand = "Nike";
+$shoe1->shoeColour = "Black and White;
+$shoe1->shoeModel = "Jordan Max Aura 2";
+$shoe1->shoeNumber = "45";
+
+
+
+$shoe2 = new type();
+$shoe2->brand = "Adidas";
+$shoe2->shoeColour = "Black";
+$shoe2->shoeModel = "Stan Smith";
+$shoe2->shoeNumber = "41";
+
  ?>
